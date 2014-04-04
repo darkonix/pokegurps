7bcc4bf3946c4206bfb4a49e4e9d15d52aa8cfa3
Mycke Ramos <mramosmanhaes@gmail.com>
2014-04-03 21:05:42 -0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pokegurps::Application.config.secret_key_base = '247c462521714d46e22f43d942949a7e5f318c35bd71c18a74b09e5a164ee8fc63d1deb81a82b9d5496ca88dac1c887e21f9e62e0321a5e985f22f411d549571'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pokegurps::Application.config.session_store :cookie_store, key: '_pokegurps_session'
